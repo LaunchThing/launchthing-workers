38cdd57da6a5c50ead9fb636178572b2ad48bd8c
Andy Triggs <andy.triggs@gmail.com>
2012-11-27 20:02:56 +0000

    Simplified actions.

--- app.rb
+++ app.rb
@@ -17,23 +17,12 @@ module LaunchThing
     end
 
     post '/crawl' do
-      required = %w{crawl_url start_urls page_quota}
-    	CrawlWorker.perform_async(valid?(args(required)))
+    	CrawlWorker.perform_async(params)
     end
 
     #
     # Helpers
     #
-
-    def args(keys)
-      keys.inject({}) do |args, k|
-        args.merge(k => params[k])
-      end
-    end
-
-    def valid?(args)
-      args.values.any? { |v| v.nil? } ? error : args
-    end
 
   end
 end
